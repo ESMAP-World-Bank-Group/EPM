6ae1944ce7364d3875a2a88959f62883a3167b4c
Maelle Baronnet <mbaronnet@worldbank.org>
2025-08-04 23:04:20 +0200

    interco sensitivities

--- epm/epm.py
+++ epm/epm.py
@@ -803,12 +803,12 @@ def perform_sensitivity(sensitivity, s):
                     #    'VREForecastError': [0, 0.3],
                     #    'ReserveVoLL': [60000, 100000] 
                     #    'SpinReserveVoLL': [100], 
-                        'costSurplus': [5, 100, 500], 
-                        'costcurtail': [1, 10], 
+                    #    'costSurplus': [5, 100, 500], 
+                    #    'costcurtail': [1, 10], 
                      #   'DR': [0.09, 0.11],
-                     #  'includeIntercoReserves': [0], 
-                      # 'interco_reserve_contribution': [0, 0.5], 
-                      #  'CapCreditSto': [0, 0.5]
+                       'includeIntercoReserves': [0], 
+                       'interco_reserve_contribution': [0, 0.5], 
+                        'CapCreditSto': [0.75, 0.5]
                         }
 
         # Iterate over the Settings to change
